463f23658e7880cff3fcd32abc1dbc229e408d08
Ivan <gladkott@gmail.com>
2021-05-14 19:15:46 +0700

    Lesson 2

---
+++ Lesson_2/app/src/main/java/com/example/kotlin_lesson_1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.kotlin_lesson_1
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    private var number : Int = 0
+    private var name: String = "Иван"
+    var sum:Int = GetNum()
+    private var text: String = "Привет, $name, ты наш $sum пользователь"
+    private var Tvtext: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Tvtext = findViewById(R.id.TvText)
+        var sum:Int = GetNum()
+        Tvtext?.setText(text)
+
+
+
+}
+    private fun GetNum():Int
+    {
+        return 1
+    }
+}
