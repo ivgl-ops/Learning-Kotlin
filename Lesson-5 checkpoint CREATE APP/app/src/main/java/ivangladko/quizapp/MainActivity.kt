304efc706c5ce6d0c933d3e1eeea99618a8e8c3f
Ivan <gladkott@gmail.com>
2021-06-04 19:26:49 +0700

    Create Admob!

--- Lesson-5 checkpoint CREATE APP/app/src/main/java/ivangladko/quizapp/MainActivity.kt
+++ Lesson-5 checkpoint CREATE APP/app/src/main/java/ivangladko/quizapp/MainActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
+
     fun clickStart(view: View)
     {
         val intent = Intent(this, firstView::class.java)
