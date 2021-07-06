12b518a5e848664982b5d40c06f5bd01bbd71122
Ivan <gladkott@gmail.com>
2021-07-06 19:58:27 +0700

    weather app

---
+++ WeatherApp2/app/src/main/java/com/example/weatherapp/api/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.api
+
+import com.example.weatherapp.model.Weather
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("weather/Luanda")
+    suspend fun getWeather (): Response<Weather>
+}
