6486cd2ba7e5e0691b16bf587bec2c2c9d42430a
Ivan <gladkott@gmail.com>
2021-07-21 20:42:51 +0700

    Aga

--- Equationsolver/app/src/main/java/ivangladko/equationsolver/MainActivity.kt
+++ Equationsolver/app/src/main/java/ivangladko/equationsolver/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import org.w3c.dom.Text
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ShowToast", "WrongViewCast")
@@ -15,10 +16,20 @@ class MainActivity : AppCompatActivity() {
         var num1 = findViewById<EditText>(R.id.num1)
         var num2 = findViewById<EditText>(R.id.num2)
         var num3 = findViewById<EditText>(R.id.num3)
-
+        val tvText = findViewById<TextView>(R.id.tvText)
+        var a: Double
+        var b: Double
+        var c: Double
+        var d: Double
+        var x1: Double
+        var x2: Double
         btn.setOnClickListener {
-            var a = Double.pa
-            val toast = Toast.makeText(applicationContext, "Процесс запущен ${num1.text}", Toast.LENGTH_SHORT )
+            a = num1.text.toString().toDouble()
+            b = num2.text.toString().toDouble()
+            c = num3.text.toString().toDouble()
+            d = (b.pow(2.0) - 4 * a * c)
+            tvText.setText(d.toString())
+            val toast = Toast.makeText(applicationContext, "Процесс запущен $a", Toast.LENGTH_SHORT )
             toast.show()
         }
 
